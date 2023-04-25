616f9775c045e7e8916aa1acbc28a9ac84b60304
Po-Chun,Yeh <111834126+ImitatedSky@users.noreply.github.com>
2023-04-25 11:07:59 +0800

    Add files via upload

---
+++ _Capture/_Capture/Capture.Designer.cs
@@ -0,0 +1,156 @@
+﻿
+namespace _Capture
+{
+    partial class Capture
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageBox = new Emgu.CV.UI.ImageBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.oKToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eXITToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.iMAGEToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sETUPToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripComboBox_Com = new System.Windows.Forms.ToolStripComboBox();
+            this.check_DataReceived = new System.Windows.Forms.CheckBox();
+            this.check_snap = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // imageBox
+            // 
+            this.imageBox.Location = new System.Drawing.Point(0, 30);
+            this.imageBox.Margin = new System.Windows.Forms.Padding(4);
+            this.imageBox.Name = "imageBox";
+            this.imageBox.Size = new System.Drawing.Size(853, 600);
+            this.imageBox.TabIndex = 2;
+            this.imageBox.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.oKToolStripMenuItem,
+            this.eXITToolStripMenuItem,
+            this.sETUPToolStripMenuItem,
+            this.iMAGEToolStripMenuItem,
+            this.toolStripComboBox_Com});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(856, 31);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // oKToolStripMenuItem
+            // 
+            this.oKToolStripMenuItem.Name = "oKToolStripMenuItem";
+            this.oKToolStripMenuItem.Size = new System.Drawing.Size(44, 27);
+            this.oKToolStripMenuItem.Text = "OK";
+            this.oKToolStripMenuItem.Click += new System.EventHandler(this.oKToolStripMenuItem_Click);
+            // 
+            // eXITToolStripMenuItem
+            // 
+            this.eXITToolStripMenuItem.Name = "eXITToolStripMenuItem";
+            this.eXITToolStripMenuItem.Size = new System.Drawing.Size(53, 27);
+            this.eXITToolStripMenuItem.Text = "EXIT";
+            this.eXITToolStripMenuItem.Click += new System.EventHandler(this.eXITToolStripMenuItem_Click);
+            // 
+            // iMAGEToolStripMenuItem
+            // 
+            this.iMAGEToolStripMenuItem.Name = "iMAGEToolStripMenuItem";
+            this.iMAGEToolStripMenuItem.Size = new System.Drawing.Size(70, 27);
+            this.iMAGEToolStripMenuItem.Text = "IMAGE";
+            // 
+            // sETUPToolStripMenuItem
+            // 
+            this.sETUPToolStripMenuItem.Name = "sETUPToolStripMenuItem";
+            this.sETUPToolStripMenuItem.Size = new System.Drawing.Size(68, 27);
+            this.sETUPToolStripMenuItem.Text = "SETUP";
+            this.sETUPToolStripMenuItem.Click += new System.EventHandler(this.sETUPToolStripMenuItem_Click);
+            // 
+            // toolStripComboBox_Com
+            // 
+            this.toolStripComboBox_Com.Name = "toolStripComboBox_Com";
+            this.toolStripComboBox_Com.Size = new System.Drawing.Size(121, 27);
+            this.toolStripComboBox_Com.SelectedIndexChanged += new System.EventHandler(this.toolStripComboBox_Com_SelectedIndexChanged);
+            // 
+            // check_DataReceived
+            // 
+            this.check_DataReceived.AutoSize = true;
+            this.check_DataReceived.Location = new System.Drawing.Point(393, 6);
+            this.check_DataReceived.Name = "check_DataReceived";
+            this.check_DataReceived.Size = new System.Drawing.Size(18, 17);
+            this.check_DataReceived.TabIndex = 4;
+            this.check_DataReceived.UseVisualStyleBackColor = true;
+            // 
+            // check_snap
+            // 
+            this.check_snap.AutoSize = true;
+            this.check_snap.Location = new System.Drawing.Point(427, 6);
+            this.check_snap.Name = "check_snap";
+            this.check_snap.Size = new System.Drawing.Size(18, 17);
+            this.check_snap.TabIndex = 5;
+            this.check_snap.UseVisualStyleBackColor = true;
+            // 
+            // Capture
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(856, 632);
+            this.Controls.Add(this.check_snap);
+            this.Controls.Add(this.check_DataReceived);
+            this.Controls.Add(this.imageBox);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Capture";
+            this.Text = "Capture";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Capture_FormClosing);
+            this.Load += new System.EventHandler(this.Capture_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox imageBox;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem oKToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eXITToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem iMAGEToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sETUPToolStripMenuItem;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox_Com;
+        private System.Windows.Forms.CheckBox check_DataReceived;
+        private System.Windows.Forms.CheckBox check_snap;
+    }
+}
+
---
+++ _Capture/_Capture/Capture.cs
@@ -0,0 +1,229 @@
+﻿using DirectShowLib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using emgucv = Emgu.CV;
+using Emgu.CV.UI;
+using Emgu.CV.Structure;
+
+namespace _Capture
+{
+    public partial class Capture : Form
+    {
+        SerialPort port;
+
+        emgucv.VideoCapture cap = null;
+        emgucv.Mat mat = null;
+
+        public Capture()
+        {
+            InitializeComponent();
+        }
+
+        private void Capture_Load(object sender, EventArgs e)
+        {
+            RefreshComport();
+
+            port = new SerialPort();
+            port.BaudRate = 115200;
+            port.Parity = Parity.None;
+            port.DataBits = 8;
+            port.StopBits = StopBits.One;
+
+            if (toolStripComboBox_Com.Items.Count > 0)
+            {
+                string comSaved = "";
+
+                string filePath = Environment.CurrentDirectory + @"\data.txt";
+                using (StreamReader sr = File.OpenText(filePath))
+                {
+                    string setting = "";
+                    while ((setting = sr.ReadLine()) != null)
+                    {
+                        switch (setting.Split('_')[0])
+                        {
+                            case "com":
+                                comSaved = setting.Split('_')[1];
+                                break;
+                        }
+                    }
+                }
+
+                if (!string.IsNullOrEmpty(comSaved))
+                    toolStripComboBox_Com.SelectedItem = comSaved;
+                else
+                    toolStripComboBox_Com.SelectedIndex = 0;
+            }
+            else
+                MessageBox.Show("未開啟藍芽通道。\n無法使用裝置Snap功能。", "無法連接藍芽", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            DsDevice[] devices = DsDevice.GetDevicesOfCat(FilterCategory.VideoInputDevice);
+            int camIndex = 0;
+            for (int i = devices.Length - 1; -1 < i; i--)
+            {
+                //以裝置名稱取得裝置序號
+                if (devices[i].Name == "USB Camera")
+                {
+                    camIndex = i;
+                    break;
+                }
+
+                //搜尋後未找到裝置
+                if (i == devices.Length - 1)
+                {
+                    MessageBox.Show("USB未連接", "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    Close();
+                    return;
+                }
+            }
+
+            cap = new emgucv.VideoCapture(camIndex);
+            cap.Set(emgucv.CvEnum.CapProp.FrameWidth, 640);
+            cap.Set(emgucv.CvEnum.CapProp.FrameHeight, 480);
+            cap.Set(emgucv.CvEnum.CapProp.Fps, 30);
+            mat = new emgucv.Mat();
+            cap.ImageGrabbed += ImageGrabbed;
+            cap.Start();
+        }
+
+        private void ImageGrabbed(object sender, EventArgs e)
+        {
+            cap.Retrieve(mat);
+            imageBox.Image = mat;
+        }
+
+        private void oKToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //imageBox.Image.Save("Fvcap.bmp");
+            Close();
+        }
+
+        private void eXITToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void sETUPToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            cap.Set(emgucv.CvEnum.CapProp.Settings, 1);
+        }
+
+        private void nORMALToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (cap == null)
+                return;
+
+            cap.FlipHorizontal = false;
+        }
+
+        private void mIRRORToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (cap == null)
+                return;
+
+            cap.FlipHorizontal = true;
+        }
+
+        private void toolStripComboBox_Com_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                string filePath = Environment.CurrentDirectory + @"\data.txt";
+                using (StreamWriter sw = File.CreateText(filePath))
+                    sw.WriteLine("com_" + (string)toolStripComboBox_Com.SelectedItem);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            PortConnect();
+        }
+
+        private void Capture_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Dispose();
+            GC.Collect();
+        }
+
+        private void DataReceived()
+        {
+            check_DataReceived.Checked = true;
+        }
+
+        private void snap()
+        {
+            check_snap.Checked = true;
+        }
+
+        private void RefreshComport()
+        {
+            //取得通訊埠，並排列順序
+            toolStripComboBox_Com.Items.Clear();
+            string[] serialPorts = SerialPort.GetPortNames();
+            foreach (string serialPort in serialPorts)
+                toolStripComboBox_Com.Items.Add(serialPort);
+        }
+
+        private void PortDataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            Invoke(new Action(() => { DataReceived(); }));
+
+            Thread.Sleep(10);
+            byte[] receivedData = new byte[5];
+            int timeCostCounter = 0;
+            while (port.BytesToRead < receivedData.Length)
+            {
+                if (timeCostCounter <= 200)
+                {
+                    Thread.Sleep(5);
+                    timeCostCounter++;
+                }
+                else
+                    break;
+            }
+
+            port.Read(receivedData, 0, receivedData.Length);
+            port.DiscardInBuffer();
+
+            if (receivedData[0] == 0x05 && receivedData[1] == 0x02 && receivedData[2] == 0x04 && receivedData[3] == 0x02 && receivedData[4] == 0x00)
+            {
+                Invoke(new Action(() => { snap(); }));
+                Invoke(new Action(() => { oKToolStripMenuItem_Click(sender, new EventArgs()); }));
+            }
+        }
+
+        private void PortConnect()
+        {
+            ConnectPort:
+            try
+            {
+                if (port.IsOpen)
+                    port.Close();
+                port.PortName = (string)toolStripComboBox_Com.SelectedItem;
+                port.Open();
+                port.DataReceived += new SerialDataReceivedEventHandler(PortDataReceived);
+            }
+            catch
+            {
+                if (toolStripComboBox_Com.SelectedIndex == toolStripComboBox_Com.Items.Count - 1)
+                    MessageBox.Show("未開啟藍芽通道或已被其他程式占用。\n無法使用裝置Snap功能。", "無法連接藍芽", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                else
+                {
+                    toolStripComboBox_Com.SelectedIndex++;
+                    goto ConnectPort;
+                }
+            }
+        }
+    }
+}
+
